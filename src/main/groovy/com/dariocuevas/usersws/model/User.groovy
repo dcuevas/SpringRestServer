4ca2fc9eff49a13374394b6aaa6c6653efd78aec
Dario <dario.cuevas@gmail.com>
2012-02-11 20:26:37 +0100

    Use User as groovy bean instead of a java plain pojo.

--- src/main/groovy/com/dariocuevas/usersws/model/User.groovy
+++ src/main/groovy/com/dariocuevas/usersws/model/User.groovy
@@ -1,4 +1,4 @@
-package com.dariocuevas.usersws.model;
+package com.dariocuevas.usersws.model
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -6,48 +6,29 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAccessType;
 
-@XmlRootElement
 @Entity
 @Table (name="Users")
-public class User {
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement
+class User {
 	
 	@Id
 	@GeneratedValue (strategy=GenerationType.IDENTITY)
 	@Column (name="id" , unique = true, nullable = false)
-	private Long id;
-	
+	@XmlElement
+	Long id
+
 	@Column (name="name")
-	private String name;
+	@XmlElement
+	String name
 	
 	@Column (name="mail")
-	private String mail;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getMail() {
-		return mail;
-	}
-
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-	
-	
+	@XmlElement
+	String mail
 
 }
